f20e7216c32eb27693bdf8944b0427c74eaf54c0
Lucas Gerisztein <lucas@lucasg.com.br>
2014-09-02 18:38:52 -0300

    Attribute variables into the scope

--- app/system/directives/directives.js
+++ app/system/directives/directives.js
@@ -3,14 +3,19 @@ app.directive('clickWeather', [function ($scope, $http) {
 	return {
 		restrict: 'E',
 		replace: true,
-		template: '<div><span class="icon-{{ weather.weather[0].icon }}"></span><span class="cw_name">{{ weather.name }}<p>{{ weather.weather[0].description }}</p></span><span class="cw_temp">{{ weather.main.temp | number:1 }}</span></div>',
+		template: '<div><span class="icon-{{ icon }}"></span><span class="cw_name">{{ city }}<p>{{ description }}</p></span><span class="cw_temp">{{ temperature | number:1 }}</span></div>',
 		controller: ['$scope', '$http', function($scope, $http) {
 	    	$scope.getTemp = function(city, unit, lang) {
 	        	$http({
 	          		method: 'GET',
 	          		url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=' + unit + '&lang=' + lang
 	        	}).success(function(data) {
-	        		$scope.weather = data;
+	        		
+	        		// Attribute specific data to variables into the scope
+	        		$scope.icon = data.weather[0].icon;
+	        		$scope.city = data.name;
+	        		$scope.description = data.weather[0].description;
+	        		$scope.temperature = data.main.temp;
 	        		
 	          	});
 	        }
